c7e366719574a5a2cf92c84d663f2bdf4751b6c0
Md Abdus Sabur <absabur929@gmail.com>
2025-09-13 16:04:16 +0600

    add space between sections

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import TabSection from "./Components/Sections/TabSection";
 
 function App() {
   return (
-    <div className="px-[10px] sm:px-[60px] container mx-auto max-w-[1480px] my-10">
+    <div className="px-[10px] sm:px-[60px] container mx-auto max-w-[1480px] my-10 flex flex-col gap-15">
       <TabSection />
       <FaqItems />
     </div>
